2c075d4b6d73f2bbbe75802156e231e0d6f45f97
Ngô T A Thư <104610812+SuperYogurt404@users.noreply.github.com>
2023-12-21 10:17:43 +0700

    thu update prj3

---
+++ DisplayPNG/MyApplication_3/STM32CubeIDE/Debug/Application/User/generated/image_resize_prj_1_nhunG.d
@@ -0,0 +1,4 @@
+Application/User/generated/image_resize_prj_1_nhunG.o: \
+ C:/TouchGFXProjects/MyApplication_3/TouchGFX/generated/images/src/image_resize_prj_1_nhunG.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
